a2fd378316e329e282147411356b4e8ebdfbf43d
sancaryum <yasineryigit737@gmail.com>
2021-02-25 02:30:22 +0300

    Mercedes added

--- app/src/main/java/com/ossovita/recyclerviewpractise/MainActivity.java
+++ app/src/main/java/com/ossovita/recyclerviewpractise/MainActivity.java
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         cars.add(new Car(R.drawable.lamborghini,"Lamborghini","2020"));
         cars.add(new Car(R.drawable.bmwlogo,"BMW","2018"));
         cars.add(new Car(R.drawable.honda,"Honda","2020"));
+        cars.add(new Car(R.drawable.mercedes,"Mercedes","2014"));
 
     }
 
